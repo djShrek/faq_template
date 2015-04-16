580656a03b323c5b1bbb3a735a2390aaae157d39
djShrek <boho527@gmail.com>
2015-04-16 16:39:50 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+    /*** Setup ***/
+    var MqM = 768, // media query medium size
+        MqL = 1026;  // media query large size
+
+    var faqsSections = $('.cd-faq-group'),
+        faqTrigger = $('.cd-faq-trigger'),
+        faqsContainer = $('.cd-faq-items'),
+        faqsCategoriesContainer = $('.cd-faq-categories'),
+        faqsCategories = faqsCategoriesContainer.find('a'),
+        closeFaqsContainer = $('.cd-close-panel');
+    /*** Setup End ***/
+});
