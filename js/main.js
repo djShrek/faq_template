159d9f1329453a78f15217a4808bb38ed26fb8a8
djShrek <boho527@gmail.com>
2015-04-16 22:23:47 -0700

    second part of function

--- js/main.js
+++ js/main.js
@@ -10,4 +10,24 @@ jQuery(document).ready(function($){
         faqsCategories = faqsCategoriesContainer.find('a'),
         closeFaqsContainer = $('.cd-close-panel');
     /*** Setup End ***/
+
+    /*** Select a faq section ***/
+        faqsCategories.on('click', function(event){ // when any of the links are clicked
+            event.preventDefault();
+            var selectedHref = $(this).attr('href'), // actual location of the link ex: #basics
+                target       = $(selectedHref); // jquery wrapped object ex: ul#basics
+            if ( $(window).width() < MqM ) {
+                faqsContainer.scrollTop(0)
+                    .addClass('slide-in').children('ul')
+                    .removeClass('selected')
+                    .end().children(selectedHref)
+                    .addClass('selected');
+                closeFaqsContainer.addClas('move-left');
+                $('body').addClass('cd-overlay');
+            }
+            else {
+                $('body,html').animate({ 'scrollTop': target.offset().top - 19}, 200);
+            }
+        });
+    /*** END ***/
 });
